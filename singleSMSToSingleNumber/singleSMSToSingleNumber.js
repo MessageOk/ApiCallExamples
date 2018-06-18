1b6dc0ef9932cfff81ee885a14db6eda30a2fd6a
Adam Šenk <adam@messageok.com>
2018-06-18 15:36:26 +0200

    single SMS to single number added Readme

--- singleSMSToSingleNumber/singleSMSToSingleNumber.js
+++ singleSMSToSingleNumber/singleSMSToSingleNumber.js
@@ -1,14 +1,14 @@
 /**
  * Created by ejdy on 18.6.18.
  */
-const rp = require(request - promise); // https://github.com/request/request-promise
+const rp = require('request-promise'); // https://github.com/request/request-promise
 
 const options = {
     method: 'POST',
     uri: 'http://api.messageok.com/v1',
     headers: {
-        api-key: 'XXXYYYZZZ'
-    }
+        'x-api-key': 'XXXYYYZZZ'
+    },
     body: {
         body: 'MessageOK API Test',
         to: [
